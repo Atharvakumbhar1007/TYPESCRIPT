8a8013c49a3c9b8c556625de0936aee774d1b2dc
Atharva <atharvakumbhar631@gmail.com>
2026-03-15 11:33:10 +0530

    Introduction

---
+++ DAY2/fun1.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 10));
+//console.log(add("5","10"));
+//Since VS Code Understand typescript - you get errors here itself,
+//before the code runs
+//tsc - typescript compiler - convert Typescript to Javascript
---
+++ TASK/task.js
@@ -0,0 +1,17 @@
+const topTracks = ["Blinding Lights", "Levitating", "Stay", "Peaches"];
+const newReleases = ["Flowers", "Unholy", "Calm Down"];
+const featuredArtist = {
+    name: "The Weeknd",
+    genre: "Pop/R&B",
+    monthlyListeners: 85000000
+}
+//TASK 1
+fullPlaylist = [...topTracks, ...newReleases, ...HANUMAN CHALISA];
+console.log(fullPlaylist);
+//TASK 2
+
+fullPlaylist = [...nowplaying, ...upNext, ...queue];
+
+
+
+
