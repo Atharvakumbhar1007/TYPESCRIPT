4cdcdda5a30945049a38da77b585a4ad08e64b24
Atharva <atharvakumbhar631@gmail.com>
2026-03-21 14:04:16 +0530

    updated

--- TS_Code/dist/classes.js
+++ TS_Code/dist/classes.js
@@ -3,35 +3,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Account {
     intRate;
     bal;
-    //default property
     constructor(intRate = 8) {
         this.intRate = intRate;
         this.bal = 0;
     }
-    deposit(mat = 0) {
+    deposit(amt = 0) {
         this.bal = this.bal + amt;
     }
     withdraw(amt = 0) {
         this.bal = this.bal - amt;
     }
-    //getter (new feature)
+    // getter
     get Balance() {
         return this.bal;
     }
-    //set
-    //much better syntax
     getBalance() {
         return this.bal;
     }
 }
-const a = new Account(500);
-//acount with initial balance of 500 Rs
-console.log(a.Balance); //no need of ()
+const a = new Account(); // interest rate = 8
+a.deposit(500); // now balance = 500
+console.log(a.Balance);
 console.log(a.getBalance());
-//you are hiding the variable named "bal"
-//a.bal = 963852741; //this wrong
-//concept of Object Oriented Programming
-//Car -->field/propertues:weight, height,color,topspeed,
-// module, etc,
-//  --> Methods: Accerate (), start(),stop(),idiomatic        
 //# sourceMappingURL=classes.js.map
--- TS_Code/src/classes.ts
+++ TS_Code/src/classes.ts
@@ -1,37 +1,33 @@
 class Account {
     private readonly intRate: number;
     private bal: number;
-    //default property
+
     constructor(intRate: number = 8) {
         this.intRate = intRate;
         this.bal = 0;
     }
 
-    deposit(mat: number = 0) {
+    deposit(amt: number = 0) {
         this.bal = this.bal + amt;
     }
+
     withdraw(amt: number = 0) {
         this.bal = this.bal - amt;
     }
 
-    //getter (new feature)
+    // getter
     get Balance(): number {
         return this.bal;
     }
-    //set
-    //much better syntax
+
     getBalance() {
         return this.bal;
     }
 }
 
-const a: Account = new Account(500);
-//acount with initial balance of 500 Rs
-console.log(a.Balance); //no need of ()
+const a: Account = new Account(); // interest rate = 8
+
+a.deposit(500); // now balance = 500
+
+console.log(a.Balance);
 console.log(a.getBalance());
-//you are hiding the variable named "bal"
-//a.bal = 963852741; //this wrong
-//concept of Object Oriented Programming
-//Car -->field/propertues:weight, height,color,topspeed,
-// module, etc,
-//  --> Methods: Accerate (), start(),stop(),idiomatic        
