a0d32ae90a285e2b821a9959852452972d3e8cac
Atharva <atharvakumbhar631@gmail.com>
2026-03-22 12:50:30 +0530

    typecast

--- TS_Code/dist/ecom.js
+++ TS_Code/dist/ecom.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//use implement for interfaces,
+//use implements for interfaces,
 //extends for inheritance
 class Product {
     id;
@@ -12,20 +12,44 @@ class Product {
         this.price = price;
     }
     get Price() {
-        return this.Price;
+        return this.price;
     }
-    set Price(NewPrice) {
+    set Price(newPrice) {
         if (newPrice < 0) {
             throw new Error("Price cannot be negative");
         }
-        this.price = NewPrice;
+        this.price = newPrice;
     }
     display() {
-        console.log(`${this.name} - Rs: ${this.Price}`);
+        console.log(`${this.name} - Rs: ${this.price}`);
     }
 }
 const p = new Product(123, "Pen", 50);
 p.display();
-p.Price = -1;
+try {
+    p.Price = -1;
+}
+catch (err) {
+    //typecast
+    let errorInfo = err;
+    console.log("Error Occurred:", errorInfo.message);
+}
 p.display();
+class ProductWithSupplier extends Product {
+    supplierName;
+    constructor(id, name, price, supplierName) {
+        super(id, name, price); //Accessing base class construtor
+        this.supplierName = supplierName;
+    }
+    get Supplier() {
+        return this.supplierName;
+    }
+    set Supplier(sName) {
+        sName = sName.trim();
+        if (sName.length < 2) {
+            throw new Error("Name too short");
+        }
+        this.supplierName = sName;
+    }
+}
 //# sourceMappingURL=ecom.js.map
--- TS_Code/src/ecom.ts
+++ TS_Code/src/ecom.ts
@@ -1,15 +1,15 @@
 type ID = string | number;
-
 interface IProduct {
     id: ID;
     name: string;
     price: number;
 }
-
+//use implements for interfaces,
+//extends for inheritance
 class Product implements IProduct {
     readonly id: ID;
     name: string;
-    private price: number; // better to keep it private
+    price: number;
 
     constructor(id: ID, name: string, price: number) {
         this.id = id;
@@ -18,32 +18,45 @@ class Product implements IProduct {
     }
 
     get Price(): number {
-        return this.price; // ✅ correct
+        return this.price;
     }
-
     set Price(newPrice: number) {
         if (newPrice < 0) {
             throw new Error("Price cannot be negative");
         }
         this.price = newPrice;
     }
-
     display() {
-        console.log(`${this.name} - Rs: ${this.Price}`);
+        console.log(`${this.name} - Rs: ${this.price}`);
     }
 }
-
 const p: Product = new Product(123, "Pen", 50);
 p.display();
 try {
     p.Price = -1;
 }
 catch (err: any) {
-    //Typecast
+    //typecast
     let errorInfo: Error = (err as Error);
-    console.log("Error Occurred:", errorInfo.message)
-
+    console.log("Error Occurred:", errorInfo.message);
 }
-
 p.display();
+
+class ProductWithSupplier extends Product {
+    constructor(id: ID, name: string, price: number, private supplierName: string) {
+        super(id, name, price); //Accessing base class construtor
+    }
+    get Supplier(): string {
+        return this.supplierName;
+    }
+    set Supplier(sName: string) {
+        sName = sName.trim();
+        if (sName.length < 2) {
+            throw new Error("Name too short");
+        }
+        this.supplierName = sName;
+    }
+    //no need to write the private supplierName:string
+    //and then write this.supplierName = supplierName,
+}
 
